a15dc9699133bb79ca52a2f81a2ea68bb565df9d
Kalie <kaliecalnan@gmail.com>
2025-08-19 17:58:22 -0400

    Add ability to display pushed buttons and clear
    Add feature to display numbers and operators pushed on calculator. Also
    add ability to clear calculator by pushing AC button.

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-function add() {
+let display = document.querySelector('#display');
+let numbers = document.querySelectorAll('.numbers');
+let operators = document.querySelectorAll('#operators button');
+let equals = document.querySelector('#equals');
+let operatorPushed = false;
+let firstNumber = 0;
+let secondNumber = 0;
+let operation = '';
 
+numbers.forEach(number => {
+    number.addEventListener('click', e => {
+        if (operatorPushed === true) {
+            display.innerText = '';
+            operatorPushed = false;
+        }        
+        display.innerText += number.innerText;
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', e => {
+        if (e.target.id !== 'equals' && e.target.id !== 'clear') {
+            if (operatorPushed === false) {
+                firstNumber = +display.innerText;
+            } else {
+                secondNumber = +display.innerText;
+            }
+            display.innerText = e.target.innerText;
+            operation = e.target.id;
+            operatorPushed = true;
+        }
+        if (e.target.id === 'clear') {
+            location.reload();
+        }
+    });
+});
+
+equals.addEventListener('click', e => {
+
+});
+
+function add(number) {
+    
 }
 
 function subtract() {
@@ -11,5 +52,9 @@ function multiply() {
 }
 
 function divide() {
-    
+
+}
+
+function operate() {
+
 }
